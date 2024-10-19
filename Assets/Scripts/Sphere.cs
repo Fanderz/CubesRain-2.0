c0e8f167140bf2ef7a5c8d8fe10d501e6f8324c5
Fanderz <135126601+Fanderz@users.noreply.github.com>
2024-10-19 19:22:34 +0300

    2nd commit

--- Assets/Scripts/Sphere.cs
+++ Assets/Scripts/Sphere.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 [RequireComponent(typeof(MeshRenderer))]
 public class Sphere : BaseObject
 {
-    [SerializeField] private float _explosionForce/* = 50f*/;
-    [SerializeField] private float _explosionRadius/* = 10f*/;
+    [SerializeField] private float _explosionForce;
+    [SerializeField] private float _explosionRadius;
     [SerializeField] private float _smoothTransparencyValue;
 
-    //private float _explosionSeconds;
+    private float _minTransparency = 0.1f;
 
     private Coroutine _coroutine;
 
@@ -22,12 +22,14 @@ public class Sphere : BaseObject
 
     private void OnEnable()
     {
+        _renderer.material.color = new Color(_renderer.material.color.r, _renderer.material.color.g, _renderer.material.color.b, 1f);
         _coroutine = StartCoroutine(ChangeTransparency());
     }
 
     private void OnDisable()
     {
-        StopCoroutine(_coroutine);
+        if (_coroutine != null)
+            StopCoroutine(_coroutine);
     }
 
     public void SetPosition(Vector3 position) =>
@@ -35,15 +37,22 @@ public class Sphere : BaseObject
 
     private void Explode()
     {
-        GetComponent<Rigidbody>().AddExplosionForce(_explosionForce, transform.position, _explosionRadius);
+        Collider[] colliders = Physics.OverlapSphere(transform.position, _explosionRadius);
+
+        foreach (Collider collider in colliders)
+        {
+            if (collider.TryGetComponent(out Rigidbody rigidbody))
+                rigidbody.AddExplosionForce(_explosionForce, transform.position, _explosionRadius);
+        }
+
         Pool<Sphere>.OnRelease(this);
     }
 
     private IEnumerator ChangeTransparency()
     {
-        while (_renderer.material.color.a != 0f)
+        while (_renderer.material.color.a != _minTransparency)
         {
-            _renderer.material.color = new Color(_renderer.material.color.r, _renderer.material.color.g, _renderer.material.color.b, Mathf.MoveTowards(_renderer.material.color.a, 0f, _smoothTransparencyValue));
+            _renderer.material.color = new Color(_renderer.material.color.r, _renderer.material.color.g, _renderer.material.color.b, Mathf.MoveTowards(_renderer.material.color.a, _minTransparency, _smoothTransparencyValue));
 
             yield return null;
         }
